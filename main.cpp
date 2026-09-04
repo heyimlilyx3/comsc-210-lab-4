53f9d95079ef81d3ba0b2e84cbbbfd2ecefc6208
heyimlilyx3 <msalexawill@gmail.com>
2026-09-04 13:07:26 -0700

    First Milestone

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
+struct Color {
+    int red;
+    int green;
+    int blue;
+
+    Color(int r, int g, int b) : red(r), green(g), blue(b) {}
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
+    Color myColor(255, 0, 0); // test data
+    cout << "Red: " << myColor.red << ", Green: " << myColor.green << ", Blue: " << myColor.blue << endl;
     return 0;
 }
