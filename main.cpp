33cac10b776b333b3fc922d6d5acfe51ead74365
heyimlilyx3 <msalexawill@gmail.com>
2026-09-04 13:17:08 -0700

    Second Milestone

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct Color {
@@ -10,7 +11,17 @@ struct Color {
 };
 
 int main() {
-    Color myColor(255, 0, 0); // test data
-    cout << "Red: " << myColor.red << ", Green: " << myColor.green << ", Blue: " << myColor.blue << endl;
+    // Color myColor(255, 0, 0); // test data
+
+    vector<Color> colors;
+    // colors.push_back(myColor);
+    for (int i = 0; i < 10; ++i) {
+        colors.push_back(Color(i * 25, i * 25, i * 25));
+    }
+
+    for (int i = 0; i < colors.size(); ++i) {
+        cout << "Color " << i << ": (" << colors[i].red << ", " << colors[i].green << ", " << colors[i].blue << ")" << endl;
+    }
+
     return 0;
 }
